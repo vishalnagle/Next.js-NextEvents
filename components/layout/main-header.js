bbad4acd3bc41cd3ae84904eb562d16204e8a3d6
Vishal Nagle <nagle.vishal@yahoo.in>
2025-01-28 13:23:23 +0530

    All events page and header component added

---
+++ components/layout/main-header.js
@@ -0,0 +1,21 @@
+import classes from "./main-header.module.css";
+import Link from "next/link";
+
+function MainHeader() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <Link href="/">NextEvents</Link>
+      </div>
+      <nav className={classes.navigation}>
+        <ul>
+          <li>
+            <Link href="/events">Browse All Events</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainHeader;
