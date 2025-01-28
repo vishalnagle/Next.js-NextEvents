b4a58d0a6fb06c7dc06c0b1f927892d622685516
Vishal Nagle <nagle.vishal@yahoo.in>
2025-01-28 14:12:24 +0530

    Event search filter functionality added

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,22 @@
 import { Fragment } from "react";
 import { getAllEvents } from "../../dummy-data";
+import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 
 function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
   );
